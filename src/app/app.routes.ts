3e5229e74d924e15a6d4433f5700729a242dc18d
kkupschi <kkupschi@gmail.com>
2026-02-16 13:28:05 +0100

    create help-text page, add routing and header link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'help',
+        loadComponent: () =>
+            import('./pages/help-text/help-text').then((m) => m.HelpText),
+    }
+];
---
+++ src/app/pages/help-text/help-text.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-text',
+  imports: [],
+  templateUrl: './help-text.html',
+  styleUrl: './help-text.scss',
+})
+export class HelpText {
+
+}
