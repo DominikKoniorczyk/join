2e100bc516dae86cf4d5267e1e63124a54832e16
KKrntzk <sophie.kornetzki@gmail.com>
2026-02-16 12:48:39 +0100

    basic footer created

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterLogo } from './footer-logo/footer-logo';
+import { FooterNav } from './footer-nav/footer-nav';
+import { FooterPolicies } from './footer-policies/footer-policies';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FooterLogo, FooterNav, FooterPolicies],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class Footer {
-
-}
+export class Footer {}
