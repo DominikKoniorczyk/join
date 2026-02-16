7ae8b718c55c97fbc3e1c29cf68a2469eccffbe0
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-02-16 16:05:52 +0100

    Push with contact Slider

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 import { Footer } from './shared/footer/footer';
 import { Contacts } from './components/contacts/contacts';
+import { NewContactSlider } from './components/contacts/contacts-menu/new-contact-slider/new-contact-slider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Contacts],
+  imports: [RouterOutlet, Header, Footer, Contacts, NewContactSlider],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
