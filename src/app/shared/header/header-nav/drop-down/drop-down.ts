7e0f9c69454c16f87298197592229d62c52eb314
KKrntzk <sophie.kornetzki@gmail.com>
2026-02-17 13:05:17 +0100

    routerlinks added to dropdown menu

--- src/app/shared/header/header-nav/drop-down/drop-down.ts
+++ src/app/shared/header/header-nav/drop-down/drop-down.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-drop-down',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './drop-down.html',
   styleUrl: './drop-down.scss',
 })
