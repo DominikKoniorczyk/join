ea3044560e87ccb81364e4b2e762611fe321a223
GiovanniMaurilio <giovannimaurilio556@gmail.com>
2026-02-17 02:09:07 +0100

    contacte können durch das drücken auf edit berbeitet werden

--- src/app/components/contacts/contacts-menu/contacts-menu.ts
+++ src/app/components/contacts/contacts-menu/contacts-menu.ts
@@ -1,38 +1,59 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { SupabaseContactsInterface } from '../../../interfaces/supabase.interfaces';
-
-
-
 
 @Component({
   selector: 'app-contacts-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contacts-menu.html',
   styleUrls: ['./contacts-menu.scss']
 })
 export class ContactsMenu {
 
- @Input() person: SupabaseContactsInterface | null = null;
-
+  @Input() person: SupabaseContactsInterface | null = null;
 
   @Output() deleteContact = new EventEmitter<number>();
   @Output() editContact = new EventEmitter<SupabaseContactsInterface>();
 
- onEdit() {
-  if (!this.person) return;
+  
+  isEditing = false;
+  editableContact: SupabaseContactsInterface | null = null;
 
-  this.editContact.emit(this.person);
-}
 
-onDelete() {
-  if (!this.person) return;
+  onEdit() {
+    if (!this.person) return;
 
-  const confirmDelete = confirm('Willst du diesen Kontakt wirklig löscen?');
+    this.isEditing = true;
 
-  if (confirmDelete) {
-    this.deleteContact.emit(this.person.id);
+    
+    this.editableContact = { ...this.person };
   }
-}
+
+
+  onSave() {
+    if (!this.editableContact) return;
+
+    this.editContact.emit(this.editableContact);
+    this.isEditing = false;
+  }
+
+
+  onCancel() {
+    this.isEditing = false;
+    this.editableContact = null;
+  }
+
+
+  onDelete() {
+    if (!this.person) return;
+
+    const confirmDelete = confirm('Willt du diesen Kontakt wirklich löscen?');
+
+    if (confirmDelete) {
+      this.deleteContact.emit(this.person.id);
+    }
+  }
+
 }
