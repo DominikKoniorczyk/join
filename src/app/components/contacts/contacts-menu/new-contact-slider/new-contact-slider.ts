f57f07f724d0dd751dd4499300e5678f30f1c82d
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-02-18 15:08:52 +0100

    refactoring of custom validators and remove supabase.spec.ts

--- src/app/components/contacts/contacts-menu/new-contact-slider/new-contact-slider.ts
+++ src/app/components/contacts/contacts-menu/new-contact-slider/new-contact-slider.ts
@@ -1,73 +1,9 @@
 import { NewContactsInterface } from './../../../../interfaces/supabase.interfaces';
-import { Component, EventEmitter, inject, Output, output, signal } from '@angular/core';
-import {
-  AbstractControl,
-  FormControl,
-  ReactiveFormsModule,
-  ValidationErrors,
-  ValidatorFn,
-  Validators,
-  FormGroup,
-} from '@angular/forms';
+import { Component, EventEmitter, inject, Output, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
 import { Supabase } from '../../../../services/supabase';
 import { InitialsPipe } from '../../../../services/contacts.services';
-
-/**
- * Validates that the input contains at least two words (e.g., first and last name),
- * each with a minimum of two characters. Supports German umlauts and "ß".
- * * @returns {ValidatorFn} A validator function that returns 'invalidContactName' if the pattern doesn't match.
- */
-export function contactNameValidator(): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const name: string = control.value;
-    if (name) {
-      const nameRegex = /^[a-zA-ZäöüÄÖÜß]{2,}\s+[a-zA-ZäöüÄÖÜß]{2,}$/;
-      const isValid = nameRegex.test(name.trim());
-      return isValid ? null : { invalidContactName: true };
-    }
-    return null;
-  };
-}
-
-/**
- * Validates the email domain structure.
- * Checks if the domain contains a dot and if the Top-Level Domain (TLD) has at least 2 characters.
- * * @returns {ValidatorFn} A validator function that returns 'email: true' if the domain suffix is too short
- * or an empty 'email: {}' object if the format is fundamentally incorrect.
- */
-export function contactEmailValidator(): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const email: string = control.value;
-
-    if (email) {
-      const domain: string = email.substring(email.lastIndexOf('@') + 1);
-      if (domain.includes('.')) {
-        return domain.substring(domain.lastIndexOf('.') + 1).length >= 2
-          ? null
-          : {
-              email: true,
-            };
-      }
-    }
-    return { email: {} };
-  };
-}
-
-/**
- * Validates the phone number input.
- * Ensures the value is not zero and has a length of more than 5 characters.
- * * @returns {ValidatorFn} A validator function that returns 'invalidPhoneNumber' if the requirements are not met.
- */
-export function contactPhoneValidator(): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const phoneNumber: number = control.value;
-    if (phoneNumber) {
-      const validNumber = phoneNumber != 0;
-      return validNumber ? null : { invalidPhoneNumber: true };
-    }
-    return null;
-  };
-}
+import { contactEmailValidator, contactNameValidator, contactPhoneValidator } from '../../../../services/custom-validators';
 
 @Component({
   selector: 'app-new-contact-slider',
