71a13a0174f959fddf1de0c3df482644293b53e0
KKrntzk <sophie.kornetzki@gmail.com>
2026-02-24 17:48:12 +0100

    dialog opening added

--- src/app/components/board/actual-board/actual-board.ts
+++ src/app/components/board/actual-board/actual-board.ts
@@ -1,15 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardCardsComponent } from './board-cards/board-cards';
+import { BoardCardsFull } from './board-cards-full/board-cards-full';
+import { Dialog } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-actual-board',
   standalone: true,
-  imports: [CommonModule, BoardCardsComponent],
+  imports: [CommonModule, BoardCardsComponent, BoardCardsFull],
   templateUrl: './actual-board.html',
   styleUrl: './actual-board.scss',
 })
 export class ActualBoard {
+
+@ViewChild('cardDialog') cardDetails! : ElementRef;
+
+
+  openDialog(){
+    this.cardDetails?.nativeElement.showModal();
+  }
 
   // @Input() task: any;
 
