d44bf707011eb2e0895c1265454a8a94ac8b9d45
KKrntzk <sophie.kornetzki@gmail.com>
2026-03-06 10:07:32 +0100

    top btns binded to supabase

--- src/app/components/summary/task-button/top-btn/top-btn.ts
+++ src/app/components/summary/task-button/top-btn/top-btn.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Task } from '../../../../interfaces/taskmodel.interfaces';
+import { Supabase } from '../../../../services/supabase';
 
 @Component({
   selector: 'app-top-btn',
@@ -6,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './top-btn.html',
   styleUrl: './top-btn.scss',
 })
-export class TopBtn {
+export class TopBtn implements OnInit {
+  tasksSignal = signal<Task[]>([]);
 
+  constructor(private supabaseService: Supabase) {}
+
+  async ngOnInit() {
+    await this.loadTasks();
+  }
+
+  async loadTasks() {
+    const data = await this.supabaseService.getDataFromTable('tasks');
+    if (data) {
+      this.tasksSignal.set(data);
+    }
+  }
+
+  getTaskCount(status: string): number {
+    return this.tasksSignal().filter(task => task.progressStatus === status).length;
+  }
 }
