1aa72f7727d1c9e51f93cd145da753225f3c92b2
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-03-10 13:32:13 +0100

    change links in footer for none logged in users

--- src/app/shared/footer/footer-nav/footer-nav.ts
+++ src/app/shared/footer/footer-nav/footer-nav.ts
@@ -1,8 +1,9 @@
-import { Component, ElementRef, ViewChild, signal } from '@angular/core';
+import { Component, ElementRef, ViewChild, computed, signal } from '@angular/core';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { filter } from 'rxjs';
 import { AuthService } from '../../../services/auth.service'; // Pfad ggf. anpassen!
 import { CommonModule } from '@angular/common';
+import { Supabase } from '../../../services/supabase';
 @Component({
   selector: 'app-footer-nav',
   standalone: true,
@@ -12,20 +13,35 @@ import { CommonModule } from '@angular/common';
 })
 export class FooterNav {
   currentURL = signal<string>("");
+  loggedIn = signal<boolean>(false);
 
-  constructor(private router: Router, public auth: AuthService) {
+  constructor(private router: Router, public auth: AuthService, private supabase: Supabase) {
     this.updateActiveURL(this.router.url);
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe(() => {
         this.updateActiveURL(this.router.url)
       });
+    this.getIsLoggedIn();
+  }
+
+  async getIsLoggedIn(){
+   const user = await this.supabase.getUser();
+   if(user) this.loggedIn.set(true);
   }
 
   updateActiveURL(newUrl: string) {
     this.currentURL.set(newUrl);
   }
+
   goTo(route: string) {
     this.router.navigate([route]);
+  }
+
+  getShowAllLinks(): boolean {
+    if (this.auth.canAccess()) {
+      return true;
+    }
+    else return false;
   }
 }
