ad40ecfb79123df8e92d49d45c39d9b88d36af8f
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-03-11 10:47:01 +0100

    add JSDocs for current-date.js

--- src/app/services/current-date.ts
+++ src/app/services/current-date.ts
@@ -4,7 +4,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CurrentDate {
-
+  /**
+   * Returns the current date in ISO format (YYYY-MM-DD).
+   *
+   * @returns {string} The current date as a string in "YYYY-MM-DD" format.
+   */
   getCurrentDate(): string {
     return new Date().toISOString().split("T")[0];
   }
