0095e45c9c09d5a3417ce0c16add7ecae08308cb
KKrntzk <sophie.kornetzki@gmail.com>
2026-03-12 12:14:41 +0100

    enviroment updated

--- src/app/environment/supabasekeys.ts
+++ src/app/environment/supabasekeys.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://ndtlhvbchxbiuaftuowb.supabase.co',
   supabaseAnonKey: 'sb_publishable_WuxJeCPnwQLmUudBbPIw8A_Ch0Wc3NV',
+  restoreDefault: false,
 };
