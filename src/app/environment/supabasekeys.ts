2da1c307bbdc38dd5de16cec268f093fe9017984
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-03-12 12:21:40 +0100

    Untrack

--- src/app/environment/supabasekeys.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://ndtlhvbchxbiuaftuowb.supabase.co',
-  supabaseAnonKey: 'sb_publishable_WuxJeCPnwQLmUudBbPIw8A_Ch0Wc3NV',
-  restoreDefault: false,
-};
