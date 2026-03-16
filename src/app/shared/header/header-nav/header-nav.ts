2161bb9b6f230c9b290fa0a0f805b7c22b784ee0
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-03-16 12:01:55 +0100

    hide help and header nav on not logged in users

--- src/app/shared/header/header-nav/header-nav.ts
+++ src/app/shared/header/header-nav/header-nav.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Help } from './help/help';
 import { DropDown } from './drop-down/drop-down';
+import { AuthService } from '../../../services/auth.service';
+import { Supabase } from '../../../services/supabase';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header-nav',
@@ -8,4 +12,27 @@ import { DropDown } from './drop-down/drop-down';
   templateUrl: './header-nav.html',
   styleUrl: './header-nav.scss',
 })
-export class HeaderNav {}
+export class HeaderNav {
+  isLoggedIn = signal<boolean>(false);
+  supaBase = inject(Supabase);
+  guest = inject(AuthService);
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe(() => {
+        this.checkIsLoggedIn();
+      });
+  }
+
+  ngAfterViewInit() {
+    this.checkIsLoggedIn();
+  }
+
+  async checkIsLoggedIn() {
+    const user = await this.supaBase.getUser();
+    if (user || this.guest.isGuestUser()) {
+      this.isLoggedIn.set(true);
+    }
+  }
+}
