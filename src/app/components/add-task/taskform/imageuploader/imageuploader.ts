00adae144514bd22b72d08c586d4c4e54198bfec
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-04-08 13:14:12 +0200

    Add Image Uploader Component

---
+++ src/app/components/add-task/taskform/imageuploader/imageuploader.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imageuploader',
+  imports: [],
+  templateUrl: './imageuploader.html',
+  styleUrl: './imageuploader.scss',
+})
+export class Imageuploader {
+
+}
